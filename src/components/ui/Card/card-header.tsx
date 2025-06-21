4a2c6680ed574274a4da7d56cd65352592ec9436
petersonwei <weipetersonwei@gmail.com>
2025-06-21 16:13:51 +1000

    chore(content): update card content to match design mockup
    - Replace placeholder content with design text

--- src/components/ui/Card/card-header.tsx
+++ src/components/ui/Card/card-header.tsx
@@ -5,7 +5,7 @@ interface CardHeaderProps {
 export default function CardHeader({ title }: CardHeaderProps) {
   return (
     <div className="px-6 pt-6">
-      <h2 className="text-2xl font-bold">
+      <h2 className="text-3xl font-extrabold">
         {title}
       </h2>
     </div>
